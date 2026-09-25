7342aaed0698227292019cd1e0b0546170c861b0
Villa Omahku <askaraindonesia201@gmail.com>
2026-09-25 21:49:59 +0700

    fix: alias net and tls modules to bypass nextjs edge restrictions

--- next.config.ts
+++ next.config.ts
@@ -14,8 +14,14 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '50mb',
     },
   },
+  // 1. ALIAS UNTUK WEBPACK
   webpack: (config, { isServer }) => {
     if (isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        net: "node:net",
+        tls: "node:tls",
+      };
       config.resolve.fallback = {
         ...config.resolve.fallback,
         "node:util/types": false,
@@ -23,7 +29,13 @@ const nextConfig: NextConfig = {
     }
     return config;
   },
-  turbopack: {}, // <-- Tambahkan baris ini untuk mengatasi error Turbopack Next.js 16
+  // 2. ALIAS UNTUK TURBOPACK (Default Next.js 16)
+  turbopack: {
+    resolveAlias: {
+      net: "node:net",
+      tls: "node:tls",
+    },
+  },
 };
 
 export default nextConfig;
